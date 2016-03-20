a66b84e844574cb2dc47db7d1e1bc730107de779
andreab <andreabqtmbc@gmail.com>
2016-03-20 18:46:58 -0500

    Cuento

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Inicio de programa");
+var personaje = prompt("Ingrese un nombre?");
+var sujeto= prompt("Ingrese un adjetivo?");
+var verbo= prompt("Ingrese un verbo");
+document.write("el cuento se trata de " +personaje);
+document.write("  esta " +sujeto );
+document.write("  y " +verbo );
+document.write("   en la plaza");
+
+document.write("<h1>Esta historia es de </h1>" +personaje);
+document.write("  es " +sujeto );
+document.write("  y " +verbo );
+document.write("   todo el día");
+
+document.write("<h3>Había una vez un sujeto llamado </h3>" +personaje);
+document.write(" y no es  " +sujeto );
+document.write(" pero " +verbo );
+document.write("   todas las mañanas");
