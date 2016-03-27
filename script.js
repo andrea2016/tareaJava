4fca99e44b9332477ad816476d4fd3edfa874800
andreab <andreabqtmbc@gmail.com>
2016-03-27 08:36:07 -0500

    modifique la pagina con colores

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 console.log("Inicio de programa");
 var personaje = prompt("Ingrese un nombre?");
 var sujeto= prompt("Ingrese un adjetivo?");
-var verbo= prompt("Ingrese un verbo");
-document.write("el cuento se trata de " +personaje);
+var verbo= prompt("Ingrese un verbo!!");
+document.write("<h3>El cuento se trata de  </h3>" +personaje);
 document.write("  esta " +sujeto );
 document.write("  y " +verbo );
 document.write("   en la plaza");
 
-document.write("<h1>Esta historia es de </h1>" +personaje);
+document.write("<h3>Esta historia es de </h3>" +personaje);
 document.write("  es " +sujeto );
-document.write("  y " +verbo );
+document.write("  y " +verbo);
 document.write("   todo el día");
 
 document.write("<h3>Había una vez un sujeto llamado </h3>" +personaje);
 document.write(" y no es  " +sujeto );
 document.write(" pero " +verbo );
 document.write("   todas las mañanas");
+document.write("<h3> Test #2 </h3>")
+
+var mensaje = ' Hola como estas  '+personaje;
+mensaje +=  '   Nos alegra que visites la pagina de nuestra organización. ';
+mensaje = mensaje + 'Si tienes dudas, anda a la pagina de contacto y mándanos un mensaje.';
+document.write(mensaje);
+
